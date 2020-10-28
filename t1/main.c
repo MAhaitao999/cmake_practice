9a7d1104f3ddf39f64846682305289510c100324
Henry Ma <haitao.ma@transwarp.io>
2020-10-28 19:47:27 +0800

    add code and book

---
+++ t1/main.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: Henry Ma
+    > Mail: iloveicRazavi@gmail.com 
+    > Created Time: 2020年10月22日 星期四 10时26分58秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    printf("Hello world from t1 Main!\n");
+    return 0;
+}
---
+++ t6/src/main.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: src/main.c
+    > Author: Henry Ma
+    > Mail: iloveicRazavi@gmail.com 
+    > Created Time: 2020年10月28日 星期三 14时27分27秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <hello.h>
+
+int main(int argc, char* argv[]) {
+
+    HelloFunc();
+    return 0;
+}
